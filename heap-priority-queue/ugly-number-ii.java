79b0a3a164744fe19d07eb5468fcf5567d070e4e
vishaa06 <140717464+vishaa06@users.noreply.github.com>
2026-08-03 23:29:17 +0530

    LC-ugly-number-ii [Runtime: N/A; Memory: N/A]

---
+++ heap-priority-queue/ugly-number-ii.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int nthUglyNumber(int n) {
+        int[] dp = new int[n];
+        dp[0] = 1;
+
+        int i2 = 0, i3 = 0, i5 = 0;
+
+        for (int i = 1; i < n; i++) {
+            int next2 = dp[i2] * 2;
+            int next3 = dp[i3] * 3;
+            int next5 = dp[i5] * 5;
+
+            int next = Math.min(next2, Math.min(next3, next5));
+            dp[i] = next;
+
+            if (next == next2) i2++;
+            if (next == next3) i3++;
+            if (next == next5) i5++;
+        }
+
+        return dp[n - 1];
+    }
+}
